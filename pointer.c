bbf43c483bd0765a9498a5eecb6713fabe04fa52
Daffu11 <f1218g@163.com>
2025-10-29 17:04:08 +0800

    Create pointer.c
    1

---
+++ pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void change(int *j)
+{
+    *j=5;
+}
+int main() {
+    int i=10;
+    printf("front change:i=%d\n",i);
+    change(&i);
+    printf("after change:i=%d\n",i);
+    return 0;
+}
